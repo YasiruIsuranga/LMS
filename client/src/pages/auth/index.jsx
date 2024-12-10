0b062466133a2c6f254b9a38d3a65e02160b07ee
YasiruIsuranga <yasiruisuranga1@gmail.com>
2024-12-10 11:01:20 +0530

    first commit

---
+++ client/src/pages/auth/index.jsx
@@ -0,0 +1,16 @@
+import { GraduationCap } from 'lucide-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function AuthPage() {
+  return <div className='flex flex-col min-h-screen'>
+    <header className='px-4 lg:px-6 h-14 flex items-center border-b'>
+      <Link to={'/'} className='flex items-center justify-center'>
+        <GraduationCap className='h-8 w-8 mr-4'/>
+        <span className='font-extrabold text-xl'>LMS LEARN</span>
+      </Link>
+    </header>
+  </div>
+}
+
+export default AuthPage
